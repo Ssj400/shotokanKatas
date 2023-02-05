d5f98d238b60648f3f840848a94080864c32e574
Ssj400 <117590185+Ssj400@users.noreply.github.com>
2023-02-05 12:09:05 -0300

    Primera versión del proyecto finalizada

---
+++ src/assets/index.js
@@ -0,0 +1,49 @@
+
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UC6psIOS7OnAlMuA3AX49pSg&part=snippet%2Cid&order=date&maxResults=50';
+
+
+const content = null || document.getElementById('content');
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'e87cdc6324mshbde1fa98bae6d8ap1bd5a1jsn98f8c0403611',
+		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+	}
+};
+
+async function fetchData(urlApi) {
+    const response = await fetch(urlApi, options);
+    const data = await response.json();
+    return data;
+}
+
+//Función anónima que se invoca a si misma 
+
+(async () => {
+    try {
+        const videos = await fetchData(API);
+        let view = `
+          ${videos.items.map(video => `
+        <a href="https://youtube.com/watch?v=${video.id.videoId}" target="_blank">
+        <div class="group relative">
+            <div
+                class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md
+                overflow-hidden group-hover:opacity-75 lg:aspect-none">
+                <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+             </div>
+             <div class="mt-4 flex-justify-between">
+                <h3 class="text-sm text-gray-700">
+                    <span aria-hidden="true" class="absolute inset-0"></span>
+                    ${video.snippet.title}
+                </h3>
+             </div>
+        </div>
+        </a>
+        `).slice(5,30).join("")}
+        `
+        content.innerHTML = view;
+    } catch(error) {
+        console.log(error);
+    }
+})();
